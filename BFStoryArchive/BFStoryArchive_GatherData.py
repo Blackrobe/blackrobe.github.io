d312ff5df5d5e4b71847f6447e1d050c0480ac6f
brianlie <brianlie_1991@yahoo.co.id>
2016-03-09 19:05:56 +0700

    Added the translated Estria story

--- BFStoryArchive/BFStoryArchive_GatherData.py
+++ BFStoryArchive/BFStoryArchive_GatherData.py
@@ -3,7 +3,7 @@ import urllib
 for i in range(1, 2):
     #urllib.urlretrieve("http://dlc.bfglobal.gumi.sg/content/event/navi_chara80%03d.png" % (i), "navi_chara80%03d.png" % (i))    
     for j in range(0, 20):
-        urllib.urlretrieve("http://dlc.bfglobal.gumi.sg/content/event/navi_chara%s_%s.png" % (i, j), "navi_chara%s_%s.png" % (i, j))
+        #urllib.urlretrieve("http://dlc.bfglobal.gumi.sg/content/event/navi_chara%s_%s.png" % (i, j), "navi_chara%s_%s.png" % (i, j))
 
 #for i in range(0, 20):
     #urllib.urlretrieve("http://dlc.bfglobal.gumi.sg/content/event/navi_chara35_%s.png" % (i), "navi_chara35_%s.png" % (i))
